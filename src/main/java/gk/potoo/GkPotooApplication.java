9cb89847da66cc1dc9ed5c6f6ffd4fa51992f88d
George Karanikas <gkaran89@gmail.com>
2016-01-20 00:24:14 +0200

    Initial commit

---
+++ src/main/java/gk/potoo/GkPotooApplication.java
@@ -0,0 +1,12 @@
+package gk.potoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GkPotooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GkPotooApplication.class, args);
+	}
+}
