7bbd333915afd4e4921828348db5eb6a82dc5470
George Karanikas <gkaran89@gmail.com>
2016-03-16 22:59:42 +0200

    Fixed mongo for tests heroku (another try)

--- src/test/java/gk/potoo/config/UnitTestMongoConfig.java
+++ src/test/java/gk/potoo/config/UnitTestMongoConfig.java
@@ -2,27 +2,16 @@ package gk.potoo.config;
 
 import com.github.fakemongo.Fongo;
 import com.mongodb.Mongo;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.core.env.Environment;
-import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 
 @Profile("test")
 @Configuration
-public class UnitTestMongoConfig extends AbstractMongoConfiguration {
-
-    @Autowired
-    private Environment env;
-
-    @Override
-    protected String getDatabaseName() {
-        return env.getRequiredProperty("mongo.db.name");
-    }
+public class UnitTestMongoConfig extends MongoConfiguration {
 
     @Override
     public Mongo mongo() throws Exception {
-        return new Fongo(getDatabaseName()).getMongo();
+        return new Fongo("foo test server").getMongo();
     }
 
 }
